52015e3443b155eee3af9aefba63d1538d2a8f71
tanika_qa <t9035793131@gmail.com>
2022-11-15 16:37:33 +0400

    Initial commit

---
+++ cypress/e2e/UI-tests/mainPage.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe ('Daily forecast on index', ()=> {
+
+    beforeEach (function () {
+        cy.visit ('https://openweathermap.org/');
+        cy.fixture('weatherData').then ((data) => {
+            this.data = data;                        });
+    })
+
+    it ('Test header', function (){
+        cy.get('div .daily-container h3').should('have.text', this.data.headerForecastDay)
+        cy.xpath("//div[@class='daily-container block mobile-padding']/h3").should('have.text', this.data.headerForecastDay)
+    });
+
+    it ('Test for the number of days', function (){
+        cy.get('ul.day-list li').should('have.length', this.data.numberDays)
+        cy.xpath("//ul[@class='day-list']/li/span").should('have.length', this.data.numberDays)
+    });
+
+    it ('Test for correct dates', function (){
+
+        let now = new Date();
+        let now1 = now.toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: '2-digit'});
+        for (let i = 0; i < 7; i++) {
+            now.setDate(now.getDate() + 1);
+            now1 += now.toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: '2-digit'})
+        };
+
+        cy.xpath("//ul[@class='day-list']/li/span").should('have.text', now1)
+        cy.get('ul.day-list li span:nth-child(1)').should('have.text', now1)
+    });
+
+    it ('Test for the number of weather icon', function (){
+        cy.get('ul svg.owm-weather-icon').should('have.length', this.data.iconWeather)
+        cy.xpath("//ul//*[@class='owm-weather-icon']").should('have.length', this.data.iconWeather)
+    });
+
+    it ('Test show full forecast and return back', function (){
+        cy.get('ul.day-list li:nth-child(1)').click()
+        cy.get('ul.options-scroller li:nth-child(1)').click()
+
+        cy.xpath("//ul[@class='day-list']/li[1]").click()
+        cy.xpath("//ul[@class='options-scroller']/li[1]").click()
+    });
+})
