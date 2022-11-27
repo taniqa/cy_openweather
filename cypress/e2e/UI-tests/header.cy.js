7139dc96825f4ed6af6f18a12498ecd53193399d
tanika_qa <t9035793131@gmail.com>
2022-11-27 19:21:35 +0400

    обновление

--- cypress/e2e/UI-tests/header.cy.js
+++ cypress/e2e/UI-tests/header.cy.js
@@ -1,22 +1,22 @@
 /// <reference types="cypress" />
 
 import "cypress-real-events/support"
-import Header from "../../pageObjects/Header"; Header
+import Header from "../../pageObjects/Header"
 
 const header = new Header();
 
 /* Чек-лист - navigation Проверка меню
 - правильное наименование пунктов меню
---- унификация названий   (проверка по стилям)
+- унификация названий пунктов меню (проверка по стилям)
+- переход по ссылкам
 - открытие/закрытие выпадающего списка в меню
 - переход по ссылкам в выпадающем меню
-- переход по ссылкам, верный endpoint, правильный заголовок на странице (соответствие названия окна в зависимости от назначения страницы)
 --- нажатие с помощью мыши
 --- нажатие с помощью клавиши Enter
 - подсветка пункта меню (css оформление) при наведение мышки
  */
 
-describe ('Testing navigation', ()=> {
+describe('Testing navigation', () => {
 
     beforeEach(function () {
         cy.visit('/');
@@ -25,19 +25,96 @@ describe ('Testing navigation', ()=> {
         });
     });
 
-    it('Correct name of menu items', function () {
+    it('Correct of menu link names', function () {
 
-        header.elements.allNavMenu().each((item, index) => {
-            expect(item.text()).to.eq(this.data.navigationMainMenu[index])
+        header.elements.allNavMenuLink().each((item, index) => {
+            expect(item.text()).to.eq(this.data.navMenuLink[index])
         })
     });
 
-    it ('Unification of menu items', function () {
-        header.elements.allNavMenu().each(($el, idx) => {
+    it('Unification of menu link names', function () {
+
+        header.elements.allNavMenuLink().each(($el, idx) => {
             cy.wrap($el).should('have.css', 'color', 'rgb(242, 242, 242)')
                 .and('have.css', 'font-family', '"Space Grotesk", Arial, sans-serif')
                 .and('have.css', 'font-size', '16px')
         })
     });
 
-})
+    it('Go to the page "Guide"', function () {
+
+        header.clickNavLinkGuide()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[0])
+                                   .and('be.visible')
+    });
+
+    it('Go to the page "API"', function () {
+
+        header.clickNavLinkApi()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[1])
+                                   .and('be.visible')
+    });
+
+    it('Go to the page "Dashboard"', function () {
+
+        header.clickNavLinkDashboard()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[2])
+                                   .and('be.visible')
+    });
+
+    it('Go to the page "Marketplace"', function () {
+
+        header.clickNavLinkMarketplace()
+        header.elements.pageTitleMarketplace().should('have.text', this.data.pageTitleText[3])
+                                              .and('be.visible')
+    });
+
+    it('Go to the page "Pricing"', function () {
+
+        header.clickNavLinkPricing()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[4])
+                                   .and('be.visible')
+    });
+
+    it('Go to the page "Maps"', function () {
+
+        header.clickNavLinkMaps()
+        cy.title().should('eq', this.data.pageTitleText[5])
+    });
+
+    it('Go to the page "Our Initiatives"', function () {
+
+        header.clickNavLinkInitiatives()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[6])
+            .and('be.visible')
+    });
+
+    it('Go to the page "Partners"', function () {
+
+        header.clickNavLinkPartners()
+        header.elements.pageTitle().should('have.text', this.data.pageTitleText[7])
+                                   .and('be.visible')
+    });
+
+    it('Go to the page "Blog"', function () {
+
+        header.clickNavLinkBlog()
+        cy.title().should('eq', this.data.pageTitleText[8])
+    });
+
+    it('Go to the page "For Business"', function () {
+
+        header.clickNavLinkBusiness()
+        header.elements.pageTitleBusiness().should('have.text', this.data.pageTitleText[9])
+                                           .and('be.visible')
+        cy.title().should('eq', this.data.pageTitleText[10])
+    });
+
+    it('Go to the page "Sign In"', function () {
+
+        header.clickNavLinkSignIn()
+        header.elements.pageTitleSignIn().should('have.text', this.data.pageTitleText[11])
+            .and('be.visible')
+    });
+
+});
--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,8 +1,61 @@
 class Header {
-elements = {
-allNavMenu: () => cy.get("div#desktop-menu > ul > li > a, div#support-dropdown")
-}
+    elements = {
+        allNavMenuLink: () => cy.get('div#desktop-menu > ul > li > a, div#support-dropdown'),
+        navLinkGuide: () => cy.get('#desktop-menu  a[href*="guide"]'),
+        navLinkApi: () => cy.get('#desktop-menu  a[href*="api"]'),
+        navLinkDashboard: () => cy.get('#desktop-menu  a[href*="dashboard"]'),
+        navLinkMarketplace: () => cy.get('#desktop-menu  a[href*="marketplace"]'),
+        navLinkPricing: () => cy.get('#desktop-menu  a[href*="price"]'),
+        navLinkMaps: () => cy.get('#desktop-menu  a[href*="/weathermap"]'),
+        navLinkInitiatives: () => cy.get('#desktop-menu  a[href*="our-initiatives"]'),
+        navLinkPartners: () => cy.get('#desktop-menu  a[href*="examples"]'),
+        navLinkBlog: () => cy.get('#desktop-menu  a[href*="blog"]'),
+        navLinkBusiness: () => cy.get('#desktop-menu  a[href="https://openweather.co.uk"]'),
+        navLinkSignIn: () => cy.get('#desktop-menu  a[href*="sign_in"]'),
+
+        pageTitle: () => cy.get('h1.breadcrumb-title'),
+        pageTitleMarketplace: () => cy.get('#custom_weather_products h1'),
+        pageTitleBusiness: () => cy.get('div.mobile-padding h1'),
+        pageTitleSignIn: () => cy.get('div.sign-form h3')
+
+    }
+
+    clickNavLinkGuide() {
+        this.elements.navLinkGuide().click()
+    };
+    clickNavLinkApi() {
+        this.elements.navLinkApi().click()
+    };
+    clickNavLinkDashboard() {
+        this.elements.navLinkDashboard().click()
+    };
+    clickNavLinkMarketplace() {
+        this.elements.navLinkMarketplace().invoke('removeAttr', 'target').click()
+    };
+    clickNavLinkPricing() {
+        this.elements.navLinkPricing().click()
+    };
+    clickNavLinkMaps() {
+        this.elements.navLinkMaps().click()
+    };
+    clickNavLinkInitiatives() {
+        this.elements.navLinkInitiatives().click()
+    };
+    clickNavLinkPartners() {
+        this.elements.navLinkPartners().click()
+    };
+    clickNavLinkBlog() {
+        this.elements.navLinkBlog().invoke('removeAttr', 'target').click()
+    };
+    clickNavLinkBusiness() {
+        this.elements.navLinkBusiness().invoke('removeAttr', 'target').click()
+    };
+    clickNavLinkSignIn() {
+        this.elements.navLinkSignIn().click()
+    };
 
 
+
 }
+
 export default Header;
