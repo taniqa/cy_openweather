df6430f10cdec1cf7ce1345038d322ad09bbfed4
tanika_qa <t9035793131@gmail.com>
2022-12-19 15:48:15 +0400

    update

--- cypress/e2e/API-tests/currentWeatherData.cy.js
+++ cypress/e2e/API-tests/currentWeatherData.cy.js
@@ -3,39 +3,37 @@ describe ("API tests", () => {
 
     let latitude = "41.642";
     let longitude = "41.636";
+    let keyId = "5fc0795fcbfb2581f15b5a22efa4489e";
     let city
     let temp
     let country
+
+    const getResponseApi = () =>
+        cy.api({
+            method: 'GET',
+            url: `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${keyId}`
+        })
 
     beforeEach(function () {
         cy.viewport (300,500);
     });
 
     it('Check status 200ok', () => {
-        cy.api( {
-            method: 'GET',
-            url: `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5fc0795fcbfb2581f15b5a22efa4489e`})
-
+        getResponseApi()
             .then((response) => {
             expect(response.status).to.eq(200)
         })
     });
 
     it('Check property', () => {
-        cy.api( {
-            method: 'GET',
-            url: `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5fc0795fcbfb2581f15b5a22efa4489e`})
-
+        getResponseApi()
             .then((response) => {
                 expect(response.body).have.property('name', 'Batumi')
             })
     });
 
     it('Check contains element with key weather and value id', () => {
-        cy.api( {
-            method: 'GET',
-            url: `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5fc0795fcbfb2581f15b5a22efa4489e`})
-
+        getResponseApi()
             .then((response) => {
                 expect(response.body.weather[0]).have.property('id')
             })
@@ -43,10 +41,7 @@ describe ("API tests", () => {
 
 //получение данных из response и запись их в переменные
         it('Put data from response to varible', () => {
-            cy.api( {
-                method: 'GET',
-                url: `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5fc0795fcbfb2581f15b5a22efa4489e`})
-
+            getResponseApi()
                 .then((response) => {
                     city = response.body.name
                     temp = response.body.main.temp
